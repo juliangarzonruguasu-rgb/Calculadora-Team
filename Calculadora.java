8865dfc878baf26f1d7ea32cb93a9543aef0536d
prueba <prueba@192.168.1.46>
2026-04-05 18:20:57 +0200

    Añadida operación dividir

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println("--- CALCULADORA ---");
+    }
+
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public double restar(double a, double b) {
+        return a - b;
+    }
+
+    public double dividir(double a, double b) {
+        return a / b;
+    }
+}
+  
