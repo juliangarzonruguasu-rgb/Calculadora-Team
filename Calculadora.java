23efc2e6293cca1fca8035330a9a3dacec273592
prueba <prueba@192.168.1.46>
2026-04-05 18:27:31 +0200

    Añadido método dividir en la rama rama-division

--- Calculadora.java
+++ Calculadora.java
@@ -1,19 +1,32 @@
+package CalculadoraApp;
+
 public class Calculadora {
+	
+	public double multiplicar(double a, double b) { 
+	    return a * b; 
+	}
 
-    public static void main(String[] args) {
-        System.out.println("--- CALCULADORA ---");
-    }
+	public double sumar(double a, double b) { 
+	    return a + b; 
+	}
 
-    public double sumar(double a, double b) {
-        return a + b;
-    }
+	public double restar(double a, double b) { 
+	    return a - b; 
+	}
 
-    public double restar(double a, double b) {
-        return a - b;
-    }
+	// Método añadido para la rama division
+	public double dividir(double a, double b) { 
+	    return a / b; 
+	}
 
-    public double dividir(double a, double b) {
-        return a / b;
-    }
+	public static void main(String[] args) {
+	
+		Calculadora calc = new Calculadora(); 
+		System.out.println("--- CALCULADORA DEL JEFE A---"); 
+		System.out.println("Suma: " + calc.sumar(10, 5)); 
+		System.out.println("Multiplicación: " + calc.multiplicar(10, 6));
+		
+		// división
+		System.out.println("División: " + calc.dividir(10, 2));
+	}
 }
-  
