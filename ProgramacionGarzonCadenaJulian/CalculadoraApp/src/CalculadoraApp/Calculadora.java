a493e854527b028d63a6979ee1399bc81856e0f4
PC <juliangarzonruguasu@gmail.com>
2026-04-05 17:16:38 +0200

    Limpieza rama-Multiplicacion

--- ProgramacionGarzonCadenaJulian/CalculadoraApp/src/CalculadoraApp/Calculadora.java
+++ ProgramacionGarzonCadenaJulian/CalculadoraApp/src/CalculadoraApp/Calculadora.java
@@ -11,8 +11,8 @@ public class Calculadora {
 	public double restar(double a, double b) { return a - b; }
 	public static void main(String[] args) {
 	
-	Calculador calc = new Calculador(); 
-	System.out.println("--- INICIO DE LA CALCULADORA ---"); 
+		Calculadora calc = new Calculadora();
+	System.out.println("--- CALCULADORA DEL JEFE A  ---"); 
 	System.out.println("Suma: " + calc.sumar(10, 5)); 
 	System.out.println("Multiplicación: " + calc.multiplicar(10, 6));
 
